8deb5216e96d0e22932483ab0359b001a56b6970
oleoot <oleoot28@gmail.com>
2020-02-15 12:03:39 +0200

    new text & animations

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 new fullpage('#fullpage', {
     navigation: true,
     onLeave: (origin,destination, direction) =>{
-    const section = destination.item;
-    const title = section.querySelector("h1");
-    const tl = new TimelineMax({delay: 0.5});
-    tl.fromTo(title, 0.5, {y: "50", opacity: 0}, {y:0, opacity: 1});
+        const section = destination.item;
+        const tl = new TimelineMax({delay: 0.5});
+        const title = section.querySelector("h1");
+        const headerDescription = document.querySelector(".s1-p");
+        tl.fromTo(title, 0.5, {y: "50", opacity: 0}, {y:0, opacity: 1});
+    if(destination.index === 0){
 
 
-    if(destination.index === 1){
+    tl.fromTo(headerDescription, 0.5, {y: "50", opacity: 0}, {y:0, opacity: 1});
+
+    }
+    else if(destination.index === 1){
     const chairs = document.querySelectorAll(".chair");
     const description = document.querySelector(".description");
 
